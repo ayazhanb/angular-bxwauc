9321f26d90b25e99e67f456eb12b83c585257981
ayazhanb <ayazhan.bakhitzhan@gmail.com>
2021-02-24 14:39:51 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bxwauc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bxwauc)
---
+++ src/app/products.ts
@@ -0,0 +1,161 @@
+export const products = [
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/71QZEqQMJML.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/51DsRDY0ufL.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/71QZEqQMJML.jpg"],
+    price: 21.69,
+    id: 1,
+    categoryId: 0,
+    name: "Fantasy Mapping: Drawing Worlds Paperback",
+    description:
+      "Fantasy Mapping: Drawing Worlds teaches you the art of fantasy map-making and shows you how to draw your own eye-catching masterpiece. Fantasy maps are illustrations of fantastical and imaginary lands.",
+    rating: 4.5,
+    link:
+      "https://www.amazon.com/Fantasy-Mapping-Drawing-Wesley-Jones/dp/0988237539/ref=sr_1_2?dchild=1&keywords=fantasy&qid=1614153293&s=books&sr=1-2"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/714mdBQ5iRL.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/71UajZaKD3L.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/714mdBQ5iRL.jpg"],
+    price: 15.58,
+    id: 2,
+    name: "House in the Cerulean Sea Paperback",
+    description:
+      "A NEW YORK TIMES AND USA TODAY BESTSELLER! One of Publishers Weekly's Most Anticipated Books of Spring 2020",
+    rating: 4.7,
+    link:
+      "https://www.amazon.com/House-Cerulean-Sea-TJ-Klune/dp/1250217318/ref=sr_1_4?dchild=1&keywords=fantasy&qid=1614153543&s=books&sr=1-4"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/81lKwTp6tzL.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/91kyetoFPSL.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/81a-UDoRRNL.jpg"],
+    price: 13.07,
+    id: 3,
+    name: "Circe Paperback ",
+    description:
+      "A bold and subversive retelling of the goddess's story, this #1 New York Times bestseller is both epic and intimate in its scope, recasting the most infamous female figure from the Odyssey as a hero in her own right (Alexandra Alter, The New York Times).",
+    rating: 4.6,
+    link:
+      "https://www.amazon.com/Circe-Madeline-Miller/dp/0316556327/ref=sr_1_5?dchild=1&keywords=fantasy&qid=1614153543&s=books&sr=1-5"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/71gXa86IG6L.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/51eFrx9855L.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/71gXa86IG6L.jpg"],
+    name: "Fairy and Fantasy 2 Grayscale Coloring Book",
+    price: 13.27,
+    id: 4,
+    description:
+      "This is a GRAYSCALE coloring book which includes 24 originally created and traditionally hand drawn illustrations by the artist Christine Karron. This coloring book contains magical illustrations of ethereal fantasy creatures, beautiful fairies and ladies with flowers, cute faces accompanied by animals and birds, whimsical elves and even some male characters.",
+    rating: 4.7,
+    link:
+      "https://www.amazon.com/Fairy-Fantasy-Grayscale-Coloring-Book/dp/B08SG5PK4C/ref=sr_1_12?dchild=1&keywords=fantasy&qid=1614153543&s=books&sr=1-12"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/71U2Zg0hd8L.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/71yC7KZ6xoL.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/81FrfPqtbfL.jpg"],
+    price: 29.99,
+    id: 5,
+    name: "Final Fantasy XIV: Shadowbringers",
+    description:
+      "A gorgeous, oversized, full-color art book showcasing the artwork from the massively popular online video game Final Fantasy XIV",
+    rating: 4.9,
+    link:
+      "https://www.amazon.com/Final-Fantasy-Shadowbringers-Square-Enix/dp/1646090616/ref=sr_1_16?dchild=1&keywords=fantasy&qid=1614153543&s=books&sr=1-16"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/61+El1r7gdL.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/61RLc8QjD7L.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/61+El1r7gdL.jpg"],
+    price: 16.99,
+    name: "Norse Myths That Inspired Final Fantasy VII",
+    id: 6,
+    description:
+      "Final Fantasy is one of the most iconic and beloved videogame series in the world. Since its inception, its titles have adopted names, themes and stories from across global mythology, including the beliefs of the Vikings. This book embarks on a detailed exploration of how Norse lore in particular influenced the writing and design of Final Fantasy VII – arguably the most critically acclaimed of the franchise – and its wider compilation. Why is Midgar the political centre of the Planet, and Nibelheim the home of Cloud Strife?",
+    rating: 4.8,
+    link:
+      "https://www.amazon.com/Norse-Myths-Inspired-Final-Fantasy/dp/1838009604/ref=sr_1_19?dchild=1&keywords=fantasy&qid=1614153543&s=books&sr=1-19"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/81hb-D7mn3L.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/81O4zzn2z7L.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/81uwTCaOtML.jpg"],
+    name: "Final Fantasy XV: The Dawn of the Future",
+    price: 17.39,
+    id: 7,
+    description:
+      "A volume of all-new, character-driven stories expanding on the world of the hit video game Final Fantasy XV. This deluxe, hardcover edition includes full-color inserts featuring concept art and exclusive content.",
+    rating: 4.9,
+    link:
+      "https://www.amazon.com/Final-Fantasy-XV-Dawn-Future/dp/1646090004/ref=sr_1_21?dchild=1&keywords=fantasy&qid=1614153543&s=books&sr=1-21"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/81i7c1UpDYL.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/81+82bNEkzL.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/91cWUL9em5L.jpg"],
+    name: "The Chronicles of Narnia Box Set",
+    price: 23.99,
+    id: 8,
+    description:
+      "It is wartime and four children are evacuated to live with a professor in the country, where they make an incredible discovery. Generations of children have followed Peter, Susan, Edmund and Lucy through the wardrobe into the magical land of Narnia.",
+    rating: 4.8,
+    link:
+      "https://www.amazon.com/Chronicles-Narnia-Box-Set/dp/0007811284/ref=sr_1_34?dchild=1&keywords=fantasy&qid=1614154300&s=books&sr=1-34"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/816cmAEKzOL.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/81KBShSBpwL.jpg"
+    ],
+    main: ["https://images-na.ssl-images-amazon.com/images/I/816cmAEKzOL.jpg"],
+    name: "Fairy and Fantasy Grayscale Coloring Book",
+    price: 13.95,
+    id: 9,
+    description:
+      "This is a GRAYSCALE coloring book which includes 24 original traditionally hand drawn illustrations by the artist Christine Karron. This coloring book contains magical illustrations of ethereal fantasy creatures, beautiful fairies and princesses, a few friendly animals and birds, some cute elves, troll girl and even a cowgirl mermaid.",
+    rating: 4.7,
+    link:
+      "https://www.amazon.com/Acer-Display-Graphics-Keyboard-A515-43-R19L/dp/B07RF1XD36/ref=sr_1_1?dchild=1&keywords=laptop&qid=1613993512&sr=8-1"
+  },
+  {
+    image: [
+      "https://images-na.ssl-images-amazon.com/images/I/41mtSzZRQWL._AC_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/318mmlTmpKL._AC_.jpg"
+    ],
+    main: [
+      "https://images-na.ssl-images-amazon.com/images/I/41QUa27LFvL._AC_.jpg"
+    ],
+    name: "Black Sun (Between Earth and Sky)",
+    price: 12.87,
+    id: 10,
+    description:
+      "From the New York Times bestselling author of Star Wars: Resistance Reborn comes the first book in the Between Earth and Sky trilogy, inspired by the civilizations of the Pre-Columbian Americas and woven into a tale of celestial prophecies, political intrigue, and forbidden magic.",
+    rating: 4.7,
+    link:
+      "https://www.amazon.com/Black-Sun-Between-Earth-Sky/dp/1534437673/ref=sr_1_53?dchild=1&keywords=fantasy&qid=1614155025&sr=8-53&srs=17143709011"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
